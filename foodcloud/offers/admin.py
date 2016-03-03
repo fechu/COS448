ca3e719df052791855f1d6ab318ed61b72869711
Sandro Meier <sandro.meier@fidelisfactory.ch>
2016-03-03 15:57:50 -0500

    Add business to Admin

--- foodcloud/offers/admin.py
+++ foodcloud/offers/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from offers.models import Offer
+from offers.models import Offer, Business
 
 
 class OfferAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Offer, OfferAdmin)
+
+
+class BusinessAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Business, BusinessAdmin)
 
